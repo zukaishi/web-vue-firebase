8588afe9175e05c556b782f3417ed520e5249fbf
zukaishi <ishizuka215@gmail.com>
2020-11-08 11:25:46 +0900

    update

--- mymarkdown/src/components/Editor.vue
+++ mymarkdown/src/components/Editor.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="editor">
         <h1>エディター画面</h1>
+        <span>{{user.displayName}}</span>
+        <button @click="logout">ログアウト</button>
     </div>
 </template>
 <script>
@@ -8,6 +10,11 @@
         name: "editor",
         data() {
             return {};
+        }
+    },
+    methods: {
+        logout: funtion() {
+            firebase.auth().signOut();
         }
     };
 </script>
